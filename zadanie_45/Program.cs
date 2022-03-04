198eb882c841441c1d499d2e4c73bf13275d7f24
CreativeOZ <zagriaoleg@gmail.com>
2022-03-04 17:47:14 +0300

    45. Показать числа Фибоначчи

---
+++ zadanie_45/Program.cs
@@ -0,0 +1,16 @@
+﻿// 45. Показать числа Фибоначчи
+Console.Clear();
+Console.Write("Покажи числа Фибоначчи: ");
+int count = int.Parse(Console.ReadLine());
+int a = 1;
+int b = 1;
+int fibo = 0;
+Console.Write($"{a}\t{b}\t");
+for (int i = 0; i < count-2; i++)
+{
+    fibo = a+b;
+    a=b;
+    b=fibo;
+    Console.Write($"{fibo}\t");
+}
+Console.WriteLine();
