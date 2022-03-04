d8eeb3b9eec33fc0f77d78a51d9bb9fa192aa4f9
CreativeOZ <zagriaoleg@gmail.com>
2022-03-04 17:17:44 +0300

    43. Написать программу преобразования десятичного числа в двоичное

---
+++ zadanie_43/Program.cs
@@ -0,0 +1,6 @@
+﻿// 43. Написать программу преобразования десятичного числа в двоичное
+Console.Clear();
+Console.Write("Введите число a: ");
+int a = int.Parse(Console.ReadLine());
+string b = Convert.ToString(a, 2);
+Console.WriteLine(b);
