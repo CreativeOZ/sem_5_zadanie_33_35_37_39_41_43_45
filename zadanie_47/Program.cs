92690ca145ca84f34067db277ed4f50594ce3dd6
CreativeOZ <zagriaoleg@gmail.com>
2022-03-10 17:05:16 +0300

    Написать программу копирования массива

---
+++ zadanie_47/Program.cs
@@ -0,0 +1,16 @@
+﻿// Написать программу копирования массива
+Console.Clear();
+
+int[] array = new int[12];
+for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0, 5);
+        Console.Write(array[i] + " ");
+    }
+Console.WriteLine();
+int[] array2 = new int[12];
+for (int i = 0; i < array2.Length; i++)
+    {
+        array2[i] = array[i];
+        Console.Write(array2[i] + " ");
+    }
