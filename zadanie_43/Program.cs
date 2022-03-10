f6557abdd9633fc73240f610a3a1860be9490984
CreativeOZ <97609463+CreativeOZ@users.noreply.github.com>
2022-03-10 17:11:06 +0300

    Update Program.cs

--- zadanie_43/Program.cs
+++ zadanie_43/Program.cs
@@ -1,6 +1,25 @@
-﻿// 43. Написать программу преобразования десятичного числа в двоичное
+// 43. Написать программу преобразования десятичного числа в двоичное
 Console.Clear();
-Console.Write("Введите число a: ");
-int a = int.Parse(Console.ReadLine());
-string b = Convert.ToString(a, 2);
-Console.WriteLine(b);
+/* Console.Write("Введите число a: ");
+int a = int.Parse(Console.ReadLine()); */
+/* string b = Convert.ToString(a, 2);
+/* Console.WriteLine(b); */ // Вариант № 1.
+
+/* int b = 0; */
+/* while (a != 0)
+{
+    b = a % 2;
+    a = a / 2;
+    Console.Write($"{b}");
+} */ // Вариант № 2.
+
+int number=new Random().Next(0,100);
+Console.Write(number);
+Console.WriteLine();
+string result = " ";
+while (number!=0) {
+result=(number%2).ToString()+result;
+number=number/2;
+}
+
+Console.WriteLine(result);
